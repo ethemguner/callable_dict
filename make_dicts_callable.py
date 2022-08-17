77b3f3cca1965b6eb8e5a55985db395b49f488c1
Ethem <47331746+ethemguner@users.noreply.github.com>
2022-08-17 15:01:34 +0300

    Update make_dicts_callable.py

--- make_dicts_callable.py
+++ make_dicts_callable.py
@@ -4,13 +4,13 @@ class CallableDict:
     def __init__(self, raw_dict):
         self.initialize_attributes(raw_dict, self)
 
-        @classmethod
-        def initialize_attributes(cls, raw_dict, current_obj):
-            """Initializes attributes of current or perhaps the next object"""
-            
-            for key, val in raw_dict.items():
-                if isinstance(val, dict):
-                    new_obj = cls(val)
-                    setattr(current_obj, key, new_obj)
-                else:
-                    setattr(current_obj, key, val)
+    @classmethod
+    def initialize_attributes(cls, raw_dict, current_obj):
+        """Initializes attributes of current or perhaps the next object"""
+
+        for key, val in raw_dict.items():
+            if isinstance(val, dict):
+                new_obj = cls(val)
+                setattr(current_obj, key, new_obj)
+            else:
+                setattr(current_obj, key, val)
